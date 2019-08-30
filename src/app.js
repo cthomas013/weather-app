b6efa1ba052c9c304d3dd610fc7d06d42df36269
Cory Thomas <corythomas@Corys-MBP.attlocal.net>
2019-08-30 17:00:33 -0400

    changed homepage title; added some text to about page

--- src/app.js
+++ src/app.js
@@ -29,7 +29,7 @@ app.use(express.static(public_path))
 
 app.get('', (req, res) => {
     res.render('index', {
-        title: 'Express Homepage',
+        title: 'Weather',
         header: 'Weather',
         creator: 'Me'
     })
